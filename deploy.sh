ec6ec1c1a99b41edf983d0922db3cd2f1139b033
Florian Buerger <flo@keslcod.com>
2014-04-05 14:42:14 +0200

    Remove redundant CSS put

--- deploy.sh
+++ deploy.sh
@@ -15,14 +15,5 @@ done
 s3cmd sync --progress -M --acl-public --add-header 'Content-Encoding:gzip' dist/ s3://${BUCKET_NAME}/ --exclude '*.*' --include '*.html' --include '*.xml' --include '*.css' --verbose
 s3cmd sync --progress -M --acl-public dist/assets/img/ s3://${BUCKET_NAME}/assets/img/ --exclude '*.*' --include '*.png' --include '*.gif' --include '*.jpg' --verbose
 
-# # force CSS upload
-# grunt sass
-# find dist/assets/css -iname '*.css' -exec gzip -n --best {} +
-# for f in `find dist/assets/css -iname '*.gz'`; do
-#   mv $f ${f%.gz}
-# done
-
-# s3cmd put --recursive --progress -M --acl-public --add-header 'Content-Encoding:gzip' dist/assets/css s3://${BUCKET_NAME}/assets/
-
 # Clean up since all pages are gzipped
 grunt clean
